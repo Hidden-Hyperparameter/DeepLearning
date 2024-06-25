89646634143cbe8ecab88810eab719209484cf3b
ZhaoHanHong <hanhong.zhao@outlook.com>
2024-06-25 10:43:57 +0800

    Change VAE

--- VAE/VAE.py
+++ VAE/VAE.py
@@ -9,16 +9,23 @@ class Decoder(nn.Module):
         super().__init__()
         self.latent_size = latent_size
         self.begin_size = 12
-        self.begin = nn.Linear(self.latent_size+10,(self.begin_size*self.begin_size)*64)
+        self.begin = nn.Sequential(
+            nn.Linear(self.latent_size+10,(self.begin_size*self.begin_size)*64),
+            nn.ReLU()
+        )
         self.net = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=64,out_channels=32,kernel_size=3,padding=1),
-            nn.ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=4,padding=1),
-            nn.ConvTranspose2d(in_channels=16,out_channels=8,kernel_size=4,padding=1,stride=2),
-            nn.ConvTranspose2d(in_channels=8,out_channels=1,kernel_size=5,padding=1),
+            nn.ConvTranspose2d(in_channels=64,out_channels=64,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=64,out_channels=32,kernel_size=4,padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=4,padding=1,stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=16,out_channels=1,kernel_size=5,padding=1),
         )
 
     def forward(self,z,y):
-        y = F.one_hot(y,num_classes=10)
+        y = F.one_hot(y,num_classes=10).to(y.device)
+        # print(z.shape,y.shape)
         m = torch.cat((z,y),dim=-1)
         m = self.begin(m).reshape(-1,64,self.begin_size,self.begin_size)
         m = self.net(m)
@@ -31,14 +38,26 @@ class Encoder(nn.Module):
         self.latent_size = latent_size
         self.feature_net = nn.Sequential(
             nn.Conv2d(in_channels=1,out_channels=64,kernel_size=2),
-            nn.Conv2d(in_channels=64,out_channels=32,kernel_size=3),
-            nn.Conv2d(in_channels=32,out_channels=16,kernel_size=4,stride=2),
-            nn.Conv2d(in_channels=16,out_channels=4,kernel_size=5),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,out_channels=64,kernel_size=3),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,out_channels=32,kernel_size=4,stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32,out_channels=8,kernel_size=5),
+            nn.ReLU()
         )
-        out_dim = 196
-        self.mu_net = nn.Linear(out_dim,self.latent_size)
-        self.sigma_net = nn.Linear(out_dim,self.latent_size)
-        self.y_net = nn.Linear(out_dim,10)
+        out_dim = 392
+        self.mu_net = nn.Sequential(
+            nn.Linear(out_dim,self.latent_size),
+            nn.ReLU(),
+            nn.Linear(self.latent_size,self.latent_size)
+        )
+        self.sigma_net = nn.Sequential(
+            nn.Linear(out_dim,self.latent_size),
+            nn.ReLU(),
+            nn.Linear(self.latent_size,self.latent_size)
+        )
+        # self.y_net = nn.Linear(out_dim,10)
 
     def forward(self,x):
         batch = x.shape[0]
@@ -48,8 +67,8 @@ class Encoder(nn.Module):
             self.mu_net(features),
             self.sigma_net(features) # logstd!
         )
-        y = self.y_net(features)
-        return z,y
+        # y = self.y_net(features)
+        return z
 
 class VAE(nn.Module):
 
@@ -59,12 +78,13 @@ class VAE(nn.Module):
         self.decoder = Decoder(latent_size=self.latent_size)
         self.encoder = Encoder(latent_size=self.latent_size)
 
+        self.x_std = 0.01
+
     def get_loss(self,x,y):
-        (mean,logstd),label = self.encoder(x)
-        std = torch.exp(2*logstd)
-        kl = 0.5 * (torch.sum(std**2,dim=-1) + torch.sum(mean**2,dim=-1) - 2 * torch.sum(logstd,dim=-1))
-        sample = Normal(loc=mean,scale=std**0.5).rsample([1]).squeeze(0)
-        reconstruct = torch.sum((self.decoder(sample,y)-x)**2,dim=[1,2,3])
-        label_loss = F.cross_entropy(label,y)
-        loss = (reconstruct + kl).mean(dim=0) +  label_loss
-        return loss
+        (mean,logstd) = self.encoder(x)
+        var = torch.exp(2*logstd)
+        kl = 0.5 * (torch.sum(var,dim=-1) + torch.sum(mean**2,dim=-1) - 2 * torch.sum(logstd,dim=-1))
+        sample = Normal(loc=mean,scale=var**0.5).rsample([1]).squeeze(0).to(x.device)
+        reconstruct = torch.sum((self.decoder(sample,y)-x.squeeze(1))**2,dim=[1,2])/(2*self.x_std)
+        # label_loss = F.cross_entropy(label,y)
+        return reconstruct.mean(dim=0),kl.mean(dim=0)#,label_loss
--- VAE/train.py
+++ VAE/train.py
@@ -13,8 +13,8 @@ import torch
 import torch.nn.functional as F
 import torchvision.utils
 
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-device = 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# device = 'cpu'
 
 mnist = utils.MNIST(batch_size=128)
 train_loader = mnist.train_dataloader
@@ -22,24 +22,46 @@ valid_loader = mnist.valid_dataloader
 
 @torch.no_grad()
 def sample(model:VAE,save_dir):
-    pass
+    lables = (torch.arange(0,10).reshape([1,-1]) * torch.ones([10,1])).reshape(-1).to(device)
+    lables = lables.to(torch.long)
+    zs = torch.randn([100,model.latent_size]).to(device)
+    outs = model.decoder(zs,lables)
+    grid = torchvision.utils.make_grid(outs.reshape(-1,1,28,28).cpu(), nrow=10)
+    torchvision.utils.save_image(grid, save_dir)
 
 def train(epochs,model:VAE,optimizer,eval_interval=1):
     for epoch in range(epochs):
-        losses = []
+        reconstructions = []
+        kls = []
+        # lable_losses = []
         with tqdm(train_loader) as bar:
-            for i,(x,y) in enumerate(train_loader):
-                loss = model.get_loss(x,y)
-                losses.append(loss.item())
+            for i,(x,y) in enumerate(bar):
+                x,y = x.to(device),y.to(device)
+                rec,kl = model.get_loss(x,y)
+                # label_loss *= 100
+                reconstructions.append(rec.item())
+                kls.append(kl.item())
+                # lable_losses.append(label_loss.item())
+                
                 optimizer.zero_grad()
-                loss.backward()
+                (rec + kl).backward()
                 optimizer.step()
 
-                if i % 1000 == 0:
-                    bar.set_description('epoch {}: loss {:.4f}'.format(epoch,sum(losses[:-100])/100))
+                if i % 10 == 0:
+                    bar.set_description('epoch {}: rec {:.4f}, kl {:.4f}'.format(epoch,sum(reconstructions[-10:])/10,sum(kls[-10:])/10))#,sum(lable_losses[-10:])/10))
+        if (epoch+1) % eval_interval == 0:
+            sample(model,save_dir=os.path.join('./samples',f'epoch_{epoch+1}.png'))
+            torch.save(model,os.path.join('./samples',f'epoch_{epoch+1}.pt'))
+        
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 
 if __name__ == '__main__':
     model = VAE().to(device)
-    optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
-    sample(model,save_dir=os.path.join('./VAE/samples',f'init.png'))
-    train(100,model,optimizer,eval_interval=1)
+    print(f'The model has {count_parameters(model)} parameters.')
+
+    optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
+    sample(model,save_dir=os.path.join('./samples',f'init.png'))
+    train(100,model,optimizer,eval_interval=5)
